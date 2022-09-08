8e5c3a8475f5fec1f42a1d3ef38554a81c30cb0c
Felixdiamond <diamondfelix006@gmail.com>
2022-09-08 16:34:24 -0700

    C.6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -9,14 +9,14 @@ int main(void)
 {
 	int a;
 	long int b;
-	short int c;
+	long long int c;
 	char d;
 	float f;
 
-	printf("Size of an interger(on 64bit): %dbyte(s) (signed)", sizeof(a));
-	printf("Size of a long_interger(on 64bit): %ldbyte(s) (signed)", sizeof(b));
-	printf("Size of a short_interger(on 64bit): %hdbyte(s) (signed)", sizeof(c));
-	printf("Size of a char(on 64bit): %cbyte(s) (signed)", sizeof(d));
-	printf("Size of a float(on 64bit): %fbyte(s) (signed)", sizeof(f));
+	printf("Size of a char(on 64bit): %lu byte(s)\n", (unsigned long)sizeof(d));
+	printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(a));
+	printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(b));
+	printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(c));
+	printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(f));
 	return (0);
 }
