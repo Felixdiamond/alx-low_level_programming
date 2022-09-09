643b0f8667ade6cfc1edbcd968b6af74f4fa664f
Felixdiamond <diamondfelix006@gmail.com>
2022-09-09 16:18:52 -0700

    var002

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+*main - Entry point
+*
+*Description - prints a to z
+*
+* Return: Always 0 (Success)
+*/
+
+int main(void) 
+{
+    
+char ch;
+
+for(ch = 'a' ; ch <= 'z' ; ch++) 
+{
+      putchar(ch);
+}
+    return (0);
+}
