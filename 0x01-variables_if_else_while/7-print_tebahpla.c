6c6848af8059c9c18447a8372912e425e908f48e
Felixdiamond <diamondfelix006@gmail.com>
2022-09-09 18:46:17 -0700

    var07

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+
+/**
+*main - Entry point
+*
+*Description - Prints a - z reversed
+*
+* Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+char i;
+char hehe;
+for (i = 'a' ; i <= 'z' ; i++) 
+{
+      strrev(putchar(ch));
+} 
+putchar(hehe);
+return (0);
+}
