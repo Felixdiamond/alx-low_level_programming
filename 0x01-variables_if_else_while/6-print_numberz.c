1b8538c5e1172e37cd5ca1058a36e90530492202
Felixdiamond <diamondfelix006@gmail.com>
2022-09-09 18:26:29 -0700

    var06

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+
+/**
+*main - Entry point
+*
+*Description - Prints digits 0 - 9 using putchar
+*
+* Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+int i;
+for (i = 0; i < 10; i++)
+{
+putchar(48 + i);
+}
+putchar("\n");
+}
