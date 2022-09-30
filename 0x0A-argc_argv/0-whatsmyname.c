efeb27beb3f2d3878470bdd19405a04ed3bce570
Felixdiamond <diamondfelix006@gmail.com>
2022-09-30 16:09:23 -0700

    one piece

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -6,9 +6,8 @@
  * @argv: Array name
  * Return: 0
  */
-
-int main (int argc, char *argv[argc * 0])
+int main(int argc, char *argv[])
 {
-	printf("%s\n", argv[0]);
-	return 0;
+printf("%s\n", argv[argc * 0]);
+return (0);
 }
